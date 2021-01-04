e88f0b1f50f1b9772378be7e07f5b909f34ea4de
Melanie Tong <melaniietong@gmail.com>
2021-01-04 21:52:57 +0000

    Updated head.js and created tail function

--- head.js
+++ head.js
@@ -1,3 +1,3 @@
 const head = function(array) {
   return array[0];
-}
+};
---
+++ tail.js
@@ -0,0 +1,7 @@
+const tail = function(array) {
+  let newArr = [];
+  for (let i = 1; i < array.length; i++) {
+    newArr.push(array[i]);
+  }
+  return newArr;
+};
