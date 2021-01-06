361c9785f51aa0f38451d76897546ce04f2c3cf3
Melanie Tong <melaniietong@gmail.com>
2021-01-06 01:41:51 +0000

    New function that asserts if arrays are equal or not

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(arrOne, arrTwo) {
+  let output = true;
+
+  // Is arrays arent' the same length, false.
+  if (arrOne.length !== arrTwo.length) {
+    output = false;
+  }
+
+  // Compares each array's index to make sure they're the same, else false.
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      output = false;
+    }
+  }
+
+  // Console log if arrays are equal or not.
+  if (output === true) {
+    return `✅ Assertion Passed: [${arrOne}] === [${arrTwo}]`;
+  } else {
+    return `🛑 Assertion Failed: [${arrOne}] !== [${arrTwo}]`;
+  }
+}
