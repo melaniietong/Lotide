1089da315f8f999cc9b211a2bb72573f606762be
Melanie Tong <melaniietong@gmail.com>
2021-01-06 01:32:44 +0000

    New function that compares arrays and determines if they're identical

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const eqArrays = function(arrOne, arrTwo) {
+  let output = true;
+
+  // Is arrays arent' the same length, false.
+  if (arrOne.length !== arrTwo.length) {
+    output = false;
+  }
+
+  // Compares each array's index to make sure they're the same, else false.
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      output = false;
+    }
+  }
+
+  return output;
+}
