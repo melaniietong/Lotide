4bdc403deeb0181d1c5528389a28fdca1860633c
Melanie Tong <melaniietong@gmail.com>
2021-01-06 21:33:51 +0000

    Function finds the middle of an array

---
+++ middle.js
@@ -0,0 +1,15 @@
+const middle = function(arrList) {
+  let newArr = [];
+
+  if (arrList.length > 2) {
+    // If even
+    if (arrList.length % 2 == 0) {
+      newArr.push(arrList[arrList.length/2]-1); // Lower middle
+      newArr.push(arrList[Math.ceil(arrList.length/2)]); // Upper middle
+    } else { // If odd
+      newArr.push(arrList[Math.floor(arrList.length/2)]);
+    }
+  } 
+
+  return newArr
+}
