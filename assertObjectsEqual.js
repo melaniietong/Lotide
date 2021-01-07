809df71ea79b9568f69ee843e15455eb5c247efe
Melanie Tong <melaniietong@gmail.com>
2021-01-07 05:27:39 +0000

    Function asserts if two objects are the same or not

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -46,3 +46,13 @@ const eqObjects = function(object1, object2) {
 
   return output;
 };
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+  }
+};
