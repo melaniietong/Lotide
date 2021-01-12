12309e2e6c996cda9d6c0357f03d0973b7e3d06f
Melanie Tong <melaniietong@gmail.com>
2021-01-12 02:19:45 +0000

    Index of all functions

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const middle = require('./middle');
 const tail = require('./tail');
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  head: head,
-  middle: middle,
-  tail: tail
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  head,
+  middle,
+  tail
 };
