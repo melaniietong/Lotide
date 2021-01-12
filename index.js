3e5e324ded9fb9d02866a408c9d6b8b26fe649bf
Melanie Tong <melaniietong@gmail.com>
2021-01-12 02:18:02 +0000

    Index of all functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const head = require('./head');
+const middle = require('./middle');
+const tail = require('./tail');
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  head: head,
+  middle: middle,
+  tail: tail
+};
