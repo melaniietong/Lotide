5f3f0ccf845674bd239f1f39910417131ad3ee73
Melanie Tong <melaniietong@gmail.com>
2021-01-12 00:23:01 +0000

    Function exports now

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,8 @@
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+
 const assertArraysEqual = function(arrOne, arrTwo) {
-  let output = true;
+  assertEqual(eqArrays(arrOne, arrTwo), true);
+};
 
-  // Is arrays arent' the same length, false.
-  if (arrOne.length !== arrTwo.length) {
-    output = false;
-  } else {
-    // Compares each array's index to make sure they're the same, else false.
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        output = false;
-      }
-    }
-  }
-
-  // Console log if arrays are equal or not.
-  if (output === true) {
-    return `✅ Assertion Passed: [${arrOne}] === [${arrTwo}]`;
-  } else {
-    return `🛑 Assertion Failed: [${arrOne}] !== [${arrTwo}]`;
-  }
-}
+module.exports = assertArraysEqual;
